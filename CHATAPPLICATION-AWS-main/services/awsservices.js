da64fb04160af3af872b8f258f99d673ede26542
kanukolu <kanukolulakshmi@gmail.com>
2023-12-18 21:50:43 +0530

    imge uploading

--- CHATAPPLICATION-AWS-main/services/awsservices.js
+++ CHATAPPLICATION-AWS-main/services/awsservices.js
@@ -3,7 +3,7 @@ const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const bucketName = process.env.BUCKET_NAME;
 const s3Client = new S3Client({
-  region: "ap-south-1",
+  region: "eu-north-1",
   credentials: {
     accessKeyId: accessKeyId,
     secretAccessKey: secretAccessKey,
@@ -21,7 +21,7 @@ exports.uploadToS3 = async (image, filename) => {
             ContentType: "image/jpeg",
           };
           const data = await s3Client.send(new PutObjectCommand(uploadParams));
-          const publicUrl = `https://${uploadParams.Bucket}.s3.ap-south-1.amazonaws.com/${uploadParams.Key}`;
+          const publicUrl = `https://${uploadParams.Bucket}.s3.eu-north-1.amazonaws.com/${uploadParams.Key}`;
           return publicUrl;
           
     } catch (error) {
